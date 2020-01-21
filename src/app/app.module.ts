49125ed3812aa0f51f46f6721c625df156c05251
Osman Samad <osamad84@yahoo.com>
2020-01-21 15:06:13 -0600

    changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TransactionPanelComponent } from './transaction-panel/transaction-panel.component';
+import { DateTimeComponent } from './date-time/date-time.component';
+import { MessagePreviewComponent } from './message-preview/message-preview.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionPanelComponent,
+    DateTimeComponent,
+    MessagePreviewComponent,
+    TaskItemComponent,
+    TopNavComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule
