29fb940a4d8478cec4bd89caf2b5b966c3ec1721
Osman Samad <osamad84@yahoo.com>
2020-04-05 16:17:58 -0500

    more cleaning up

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import orders from '../orders';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-
+  orders: any [] = [];
   constructor() { }
 
   ngOnInit() {
+    this.orders = orders;
   }
 
 }
